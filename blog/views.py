96df162120e7d6ab80fd01372ca0a2bbc6a81ed7
moonstardust <spamsandother@gmail.com>
2017-07-20 10:57:28 -0700

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+def index(request):
+    return render(request, 'blog/index.html', {})
